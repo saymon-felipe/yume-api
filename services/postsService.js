5ee4d86c817d3d38c7ef6e2cb3cc3b1647a77746
Saymon Felipe <linnubr@gmail.com>
2024-02-28 21:41:48 -0300

    Cademint tarefa #475

--- services/postsService.js
+++ services/postsService.js
@@ -24,8 +24,13 @@ let postsService = {
             })
         })
     },
-    returnFeed: function (user_id, offset, limite) {
+    returnFeed: function (user_id, offset, limite, friends = false) {
         return new Promise((resolve, reject) => {
+            let friendsQuery = `
+                    JOIN
+                friends f ON (p.creator_id = f.friend1 AND f.friend2 = ${user_id}) OR (p.creator_id = f.friend2 AND f.friend1 = ${user_id})
+            `
+
             functions.executeSql(
                 `
                     CREATE TEMPORARY TABLE tmp_subquery AS
@@ -43,14 +48,22 @@ let postsService = {
                             p.reference_post_id,
                             p.reference_post_user_id,
                             u.profile_photo,
-                            u.nickname
+                            u.nickname,
+                            pm.views AS post_views,
+                            pm.engagement AS post_engagement
                         FROM
                             posts p
                         LEFT JOIN
                             users u ON p.creator_id = u.id
+                        LEFT JOIN
+                            post_metadata pm ON p.id = pm.post_id
+                        GROUP BY
+                            p.id
 
-                        LIMIT ? OFFSET ?;
+                        ${ friends ? friendsQuery : "" }
                         
+                        LIMIT ? OFFSET ?;
+
                         -- Inserção na tabela post_metadata com base nos resultados da subconsulta
                         INSERT INTO post_metadata (post_id, views, engagement)
                         SELECT
@@ -61,7 +74,7 @@ let postsService = {
                             tmp_subquery
                         ON DUPLICATE KEY UPDATE
                             views = views + 1;
-                        
+
                         SELECT
                             ts.*,
                             (SELECT COUNT(id) FROM post_likes pl WHERE pl.post_id = ts.id) AS likes,
@@ -69,8 +82,18 @@ let postsService = {
                             (SELECT COUNT(id) FROM posts where reference_post_id = ts.id) AS sharings,
                             CASE WHEN EXISTS (SELECT 1 FROM post_likes WHERE post_id = ts.id AND liker_user = ?) THEN 1 ELSE 0 END AS user_liked
                         FROM
-                            tmp_subquery ts;
-                        
+                            tmp_subquery ts
+                        ORDER BY
+                            -- Ordena primeiro os posts mais relevantes (mais engajamento em menos tempo)
+                            (ts.post_engagement / TIMESTAMPDIFF(SECOND, ts.create_date, NOW())) DESC,
+                            -- Em seguida, os mais relevantes em termos de visualizações
+                            (ts.post_views / TIMESTAMPDIFF(SECOND, ts.create_date, NOW())) DESC,
+                            -- Em seguida, os mais recentes
+                            ts.create_date DESC,
+                            -- Por fim, os mais antigos com menos visualizações
+                            ts.create_date ASC,
+                            ts.post_views ASC;
+
                         -- Remoção da tabela temporária após o uso
                         DROP TEMPORARY TABLE IF EXISTS tmp_subquery;
                 `, [parseInt(limite), parseInt(offset), user_id]
@@ -138,6 +161,10 @@ let postsService = {
                     WHERE NOT EXISTS (
                         SELECT 1 FROM post_likes WHERE post_id = ${post_id} AND liker_user = ${user_id}
                     ) LIMIT 1;
+
+                    UPDATE post_metadata
+                    SET engagement = engagement + 1
+                    WHERE post_id = ${post_id};
                 `, []
             ).then(() => {
                 resolve();
@@ -154,7 +181,11 @@ let postsService = {
                         posts
                         (creator_id, create_date, reference_post_id, reference_post_profile_photo, reference_post_nickname, reference_post_create_date, reference_post_text, reference_post_image, post_text, reference_post_user_id)
                     VALUES
-                        (?, DATE_ADD(CURRENT_TIMESTAMP(), interval -3 hour), ?, ?, ?, ?, ?, ?, ?, ?)
+                        (?, DATE_ADD(CURRENT_TIMESTAMP(), interval -3 hour), ?, ?, ?, ?, ?, ?, ?, ?);
+
+                    UPDATE post_metadata
+                    SET engagement = engagement + 1
+                    WHERE post_id = ${post_id};
                 `, [user_id, post_id, profile_photo, nickname, create_date, text, post_image, new_text, reference_user_id]
             ).then((results) => {
                 let post = {
@@ -169,6 +200,103 @@ let postsService = {
                 }
 
                 resolve(post);
+            }).catch((error) => {
+                reject(error);
+            })
+        })
+    },
+    commentInPost: function (user_id, post_id, comment) {
+        return new Promise((resolve, reject) => {
+            if (comment.length >= 2000) {
+                reject("Número de caracteres excede o permitido");
+            } else {
+                functions.executeSql(
+                    `
+                        INSERT INTO
+                            post_comments
+                            (post_id, creator_id, create_date, comment)
+                        VALUES
+                            (?, ?, DATE_ADD(CURRENT_TIMESTAMP(), interval -3 hour), ?);
+
+                        UPDATE post_metadata
+                        SET engagement = engagement + 1
+                        WHERE post_id = ${post_id};
+                    `, [post_id, user_id, comment]
+                ).then(() => {
+                    resolve();
+                }).catch((error) => {
+                    reject(error);
+                })
+            }
+        })
+    },
+    returnPostComments: function (post_id) {
+        return new Promise((resolve, reject) => {
+            functions.executeSql(
+                `
+                    SELECT
+                        pc.*,
+                        u.profile_photo,
+                        u.nickname
+                    FROM
+                        post_comments pc
+                    INNER JOIN
+                        users u ON pc.creator_id = u.id
+                    WHERE
+                        post_id = ?
+                `, [post_id]
+            ).then((results) => {
+                resolve(results);
+            }).catch((error) => {
+                reject(error);
+            })
+        })
+    },
+    excludePost: function (post_id, user_id) {
+        return new Promise((resolve, reject) => {
+            functions.executeSql(
+                `
+                    DELETE FROM
+                        posts
+                    WHERE
+                        id = ?
+                    AND
+                        creator_id = ?
+                `, [post_id, user_id]
+            ).then((results) => {
+                if (results.affectedRows == 0) {
+                    reject("Você não tem permissão para excluir este post");
+                } else {
+                    resolve();
+                }
+            }).catch((error) => {
+                reject(error);
+            })
+        })
+    },
+    editPost: function (post_id, user_id, post_text) {
+        return new Promise((resolve, reject) => {
+            functions.executeSql(
+                `
+                    UPDATE
+                        posts
+                    SET
+                        post_text = ?
+                    WHERE
+                        id = ?
+                    AND
+                        creator_id = ?;
+
+                    UPDATE post_metadata
+                    SET engagement = GREATEST(1, FLOOR(engagement / 2))
+                    WHERE post_id = ${post_id};
+                `, [post_text, post_id, user_id]
+            ).then((results) => {
+                if (results.affectedRows == 0) {
+                    reject("Você não tem permissão para editar este post");
+                } else {
+                    resolve();
+                }
             }).catch((error) => {
                 reject(error);
             })
